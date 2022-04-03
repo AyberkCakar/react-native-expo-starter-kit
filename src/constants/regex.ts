8115edac50805182c5c47fd2d4ebf8855dc4f77e
AyberkCakar <ayberkcakar35@gmail.com>
2022-04-04 02:03:56 +0300

    feat: added regex constants

---
+++ src/constants/regex.ts
@@ -0,0 +1,6 @@
+export const name: RegExp = /[a-zA-Z\ ]{3,15}/;
+
+export const email: RegExp = /^[^\s@]+@[^\s@]+\.([^\s@]{2,})+$/;
+
+export const password: RegExp =
+  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/;
