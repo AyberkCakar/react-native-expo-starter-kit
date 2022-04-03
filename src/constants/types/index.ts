e38c78777de05a94455ad43e96d80262b3857005
AyberkCakar <ayberkcakar35@gmail.com>
2022-04-04 02:06:24 +0300

    feat: added useScreenOption hooks

---
+++ src/constants/types/index.ts
@@ -0,0 +1,48 @@
+import i18n from 'i18n-js';
+import {ImageSourcePropType} from 'react-native';
+import {ITheme} from './theme';
+
+export * from './components';
+export * from './theme';
+
+export interface ILocation {
+  id?: number;
+  city?: string;
+  country?: string;
+}
+
+export interface ITranslate {
+  locale: string;
+  setLocale: (locale?: string) => void;
+  t: (scope?: i18n.Scope, options?: i18n.TranslateOptions) => string;
+  translate: (scope?: i18n.Scope, options?: i18n.TranslateOptions) => string;
+}
+export interface IExtra {
+  id?: number;
+  name?: string;
+  time?: string;
+  image: ImageSourcePropType;
+  saved?: boolean;
+  booked?: boolean;
+  available?: boolean;
+  onBook?: () => void;
+  onSave?: () => void;
+  onTimeSelect?: (id?: number) => void;
+}
+
+export interface INotification {
+  id?: number;
+  subject?: string;
+  message?: string;
+  read?: boolean;
+  business?: boolean;
+  createdAt?: number | Date;
+  type:
+    | 'document'
+    | 'documentation'
+    | 'payment'
+    | 'notification'
+    | 'profile'
+    | 'extras'
+    | 'office';
+}
---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+export {ThemeContext, ThemeProvider, default as useTheme} from './useTheme';
+export {default as useScreenOptions} from './useScreenOptions';
+export {
+  TranslationContext,
+  TranslationProvider,
+  useTranslation,
+} from './useTranslation';
