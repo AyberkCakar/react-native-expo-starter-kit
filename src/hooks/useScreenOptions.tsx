85e1194359c29aff938fffb4a17b91cb25a97993
AyberkCakar <ayberkcakar35@gmail.com>
2022-04-10 01:33:11 +0300

    refactor: useScreenOptions hook refactored.

--- src/hooks/useScreenOptions.tsx
+++ src/hooks/useScreenOptions.tsx
@@ -38,48 +38,7 @@ export default () => {
     ),
     headerRight: () => (
       <Block row flex={0} align="center" marginRight={sizes.padding}>
-        <TouchableOpacity
-          style={{marginRight: sizes.sm}}
-          onPress={() =>
-            navigation.navigate('Screens', {
-              screen: 'Pro',
-            })
-          }>
-          <Image source={icons.bell} radius={0} color={colors.icon} />
-          <Block
-            flex={0}
-            right={0}
-            width={sizes.s}
-            height={sizes.s}
-            radius={sizes.xs}
-            position="absolute"
-            gradient={gradients?.primary}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate('Screens', {
-              screen: 'Pro',
-            })
-          }>
-          <Image source={icons.basket} radius={0} color={colors.icon} />
-          <Block
-            flex={0}
-            padding={0}
-            justify="center"
-            position="absolute"
-            top={-sizes.s}
-            right={-sizes.s}
-            width={sizes.sm}
-            height={sizes.sm}
-            radius={sizes.sm / 2}
-            gradient={gradients?.primary}>
-            <Text white center bold size={10} lineHeight={10} paddingTop={3}>
-              3
-            </Text>
-          </Block>
-        </TouchableOpacity>
-      </Block>
+     </Block>
     ),
   } as StackHeaderOptions;
 
@@ -90,22 +49,6 @@ export default () => {
       headerTitle: () => (
         <Text p white>
           {t('navigation.components')}
-        </Text>
-      ),
-      headerRight: () => null,
-      headerLeft: () => (
-        <Button
-          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
-          <Image source={icons.menu} radius={0} color={colors.white} />
-        </Button>
-      ),
-    },
-    pro: {
-      ...menu,
-      headerTransparent: true,
-      headerTitle: () => (
-        <Text p white semibold>
-          {t('pro.title')}
         </Text>
       ),
       headerRight: () => null,
@@ -130,44 +73,6 @@ export default () => {
             transform={[{rotate: '180deg'}]}
           />
         </Button>
-      ),
-    },
-    profile: {
-      ...menu,
-      headerRight: () => (
-        <Block row flex={0} align="center" marginRight={sizes.padding}>
-          <TouchableOpacity
-            style={{marginRight: sizes.sm}}
-            onPress={() =>
-              navigation.navigate('Screens', {
-                screen: 'Notifications',
-              })
-            }>
-            <Image source={icons.bell} radius={0} color={colors.icon} />
-            <Block
-              flex={0}
-              right={0}
-              width={sizes.s}
-              height={sizes.s}
-              radius={sizes.xs}
-              position="absolute"
-              gradient={gradients?.primary}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() =>
-              navigation.dispatch(
-                DrawerActions.jumpTo('Screens', {screen: 'Profile'}),
-              )
-            }>
-            <Image
-              radius={6}
-              width={24}
-              height={24}
-              source={{uri: 'https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?fit=crop&w=80&q=80'}}
-            />
-          </TouchableOpacity>
-        </Block>
       ),
     },
   };
