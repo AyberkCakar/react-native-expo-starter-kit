468a38afff70123cdb52b55cf744149ed949928d
AyberkCakar <ayberkcakar35@gmail.com>
2022-04-10 01:34:51 +0300

    refactor: types index refactored.

--- src/constants/types/index.ts
+++ src/constants/types/index.ts
@@ -1,9 +1,9 @@
-import i18n from 'i18n-js';
-import {ImageSourcePropType} from 'react-native';
-import {ITheme} from './theme';
+import i18n from "i18n-js";
+import { ImageSourcePropType } from "react-native";
+import { ITheme } from "./theme";
 
-export * from './components';
-export * from './theme';
+export * from "./components";
+export * from "./theme";
 
 export interface ILocation {
   id?: number;
@@ -30,6 +30,15 @@ export interface IExtra {
   onTimeSelect?: (id?: number) => void;
 }
 
+export interface IUseData {
+  isDark: boolean;
+  handleIsDark: (isDark?: boolean) => void;
+  theme: ITheme;
+  setTheme: (theme?: ITheme) => void;
+  notifications: INotification[];
+  handleNotifications: (data?: INotification[]) => void;
+}
+
 export interface INotification {
   id?: number;
   subject?: string;
@@ -38,11 +47,11 @@ export interface INotification {
   business?: boolean;
   createdAt?: number | Date;
   type:
-    | 'document'
-    | 'documentation'
-    | 'payment'
-    | 'notification'
-    | 'profile'
-    | 'extras'
-    | 'office';
+    | "document"
+    | "documentation"
+    | "payment"
+    | "notification"
+    | "profile"
+    | "extras"
+    | "office";
 }
