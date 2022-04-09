4ae2cba5e08e39c46b3016e5741bc8fa4e1dcd1f
AyberkCakar <ayberkcakar35@gmail.com>
2022-04-10 01:35:38 +0300

    feat: added Screens navigation.

---
+++ src/navigation/Screens.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { Login, Home } from "../screens";
+import { useScreenOptions, useTranslation } from "../hooks";
+
+const Stack = createStackNavigator();
+
+export default () => {
+  const { t } = useTranslation();
+  const screenOptions = useScreenOptions();
+
+  return (
+    <Stack.Navigator screenOptions={screenOptions.stack}>
+       <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{title: t('navigation.home')}}
+      />
+
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
