c9892d49756907a0cb0f1d68cdd782d14fe0aa1c
AyberkCakar <ayberkcakar35@gmail.com>
2022-04-10 01:34:27 +0300

    feat: added home page.

---
+++ src/screens/Home.tsx
@@ -0,0 +1,25 @@
+import React, { useCallback, useState } from "react";
+
+import { useTheme, useTranslation } from "../hooks/";
+import { Block, Input } from "../components/";
+
+const Home = () => {
+  const { t } = useTranslation();
+  const { colors, sizes } = useTheme();
+
+  return (
+    <Block>
+      <Block color={colors.card} flex={0} padding={sizes.padding}>
+        <Input search placeholder={t("common.search")} />
+      </Block>
+      <Block
+        scroll
+        paddingHorizontal={sizes.padding}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: sizes.l }}
+      ></Block>
+    </Block>
+  );
+};
+
+export default Home;
