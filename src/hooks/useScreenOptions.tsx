2e0cb6d61bf7f1785f13c1dc67de3497b2c86a70
AyberkCakar <ayberkcakar35@gmail.com>
2022-04-10 20:31:24 +0300

    refactor: useScreenOptions refactored

--- src/hooks/useScreenOptions.tsx
+++ src/hooks/useScreenOptions.tsx
@@ -1,34 +1,33 @@
-import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import React from "react";
 import {
   StackHeaderTitleProps,
   CardStyleInterpolators,
-} from '@react-navigation/stack';
-import {useNavigation} from '@react-navigation/core';
-import {DrawerActions} from '@react-navigation/native';
-import {StackHeaderOptions} from '@react-navigation/stack/lib/typescript/src/types';
+} from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/core";
+import { DrawerActions } from "@react-navigation/native";
+import { StackHeaderOptions } from "@react-navigation/stack/lib/typescript/src/types";
 
-import {useTranslation} from './useTranslation';
+import { useTranslation } from "./useTranslation";
 
-import Image from '../components/Image';
-import Text from '../components/Text';
-import useTheme from '../hooks/useTheme';
-import Button from '../components/Button';
-import Block from '../components/Block';
+import Image from "../components/Image";
+import Text from "../components/Text";
+import useTheme from "../hooks/useTheme";
+import Button from "../components/Button";
+import Block from "../components/Block";
 
 export default () => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   const navigation = useNavigation();
-  const {icons, colors, gradients, sizes} = useTheme();
+  const { icons, colors, sizes } = useTheme();
 
   const menu = {
-    headerStyle: {elevation: 0},
-    headerTitleAlign: 'left',
-    headerTitleContainerStyle: {marginLeft: -sizes.sm},
-    headerLeftContainerStyle: {paddingLeft: sizes.s},
-    headerRightContainerStyle: {paddingRight: sizes.s},
+    headerStyle: { elevation: 0 },
+    headerTitleAlign: "left",
+    headerTitleContainerStyle: { marginLeft: -sizes.sm },
+    headerLeftContainerStyle: { paddingLeft: sizes.s },
+    headerRightContainerStyle: { paddingRight: sizes.s },
     cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-    headerTitle: ({children}: StackHeaderTitleProps) => (
+    headerTitle: ({ children }: StackHeaderTitleProps) => (
       <Text p>{children}</Text>
     ),
     headerLeft: () => (
@@ -37,8 +36,7 @@ export default () => {
       </Button>
     ),
     headerRight: () => (
-      <Block row flex={0} align="center" marginRight={sizes.padding}>
-     </Block>
+      <Block row flex={0} align="center" marginRight={sizes.padding}></Block>
     ),
   } as StackHeaderOptions;
 
@@ -48,13 +46,14 @@ export default () => {
       ...menu,
       headerTitle: () => (
         <Text p white>
-          {t('navigation.components')}
+          {t("navigation.components")}
         </Text>
       ),
       headerRight: () => null,
       headerLeft: () => (
         <Button
-          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+        >
           <Image source={icons.menu} radius={0} color={colors.white} />
         </Button>
       ),
@@ -70,7 +69,7 @@ export default () => {
             height={18}
             color={colors.icon}
             source={icons.arrow}
-            transform={[{rotate: '180deg'}]}
+            transform={[{ rotate: "180deg" }]}
           />
         </Button>
       ),
