137c2ca9a5767d7be40672023c327c113d07f1e9
AyberkCakar <ayberkcakar35@gmail.com>
2022-04-11 21:48:36 +0300

    fix: fixed StackActions replace error

--- src/hooks/useAuthentication.tsx
+++ src/hooks/useAuthentication.tsx
@@ -1,25 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "../../firebase";
-import { StackActions ,useNavigation} from "@react-navigation/native";
+import { StackActions, useNavigation } from "@react-navigation/native";
 
 export function useAuthentication() {
   const [user, setUser] = useState<User>();
   const navigation = useNavigation();
 
-  if (auth) {
-    const currentStack = navigation.getState().routes[0].name;
-    const navigateStack = auth.currentUser ? "Menu" : "Auth";
-
-    if (navigateStack !== currentStack) {
-        navigation.dispatch(StackActions.replace(navigateStack));
-    }
-  }
-
   useEffect(() => {
     const unsubscribeFromAuthStatuChanged = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
+        setNavigateStack();
       } else {
         setUser(undefined);
       }
@@ -27,6 +19,17 @@ export function useAuthentication() {
 
     return unsubscribeFromAuthStatuChanged;
   }, []);
+
+  async function setNavigateStack() {
+    if (auth) {
+      const currentStack = await navigation.getState().routes[0].name;
+      const navigateStack = auth.currentUser ? "Menu" : "Auth";
+
+      if (navigateStack !== currentStack) {
+        await navigation.dispatch(StackActions.replace(navigateStack));
+      }
+    }
+  }
 
   return {
     user,
