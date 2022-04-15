3c85802458e4bbbf6d01da0dc7003c7ebe3a85a9
AyberkCakar <ayberkcakar35@gmail.com>
2022-04-15 23:10:52 +0300

    feat: added buttons component examples

--- src/screens/ComponentExamples.tsx
+++ src/screens/ComponentExamples.tsx
@@ -1,43 +1,171 @@
-import React, {useLayoutEffect} from 'react';
+import React, { useLayoutEffect, useState } from "react";
+import { FlatList, StyleSheet } from "react-native";
 
-import {useNavigation} from '@react-navigation/core';
-import {useHeaderHeight} from '@react-navigation/stack';
+import { useNavigation } from "@react-navigation/core";
+import { useHeaderHeight } from "@react-navigation/stack";
 
-import {useTheme} from '../hooks/';
-import {Block, Image} from '../components/';
+import { useTheme, useTranslation } from "../hooks/";
+import { Block, Button, Image, Modal, Text } from "../components/";
 
+const Buttons = () => {
+  const [showModal, setModal] = useState(false);
+  const [quantity, setQuantity] = useState("01");
+  const { assets, colors, gradients, sizes } = useTheme();
+  const { t } = useTranslation();
 
-const ComponentExamples = () => {
-    const {assets, sizes} = useTheme();
-    const navigation = useNavigation();
-    const headerHeight = useHeaderHeight();
-  
-    useLayoutEffect(() => {
-      navigation.setOptions({
-        headerBackground: () => (
-          <Image
-            radius={0}
-            resizeMode="cover"
-            width={sizes.width}
-            height={headerHeight}
-            source={assets.header}
-          />
-        ),
-      });
-    }, [assets.header, navigation, sizes.width, headerHeight]);
-  
-    return (
-      <Block safe>
-        <Block
-          scroll
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{paddingVertical: sizes.padding}}>
-          <Block>
-          </Block>
+  return (
+    <Block paddingHorizontal={sizes.padding}>
+      <Text p semibold marginBottom={sizes.s}>
+        {t("componentExamples.buttons.buttons")}
+      </Text>
+      <Block>
+        <Button flex={1} gradient={gradients.primary} marginBottom={sizes.base}>
+          <Text white bold transform="uppercase">
+            {t("componentExamples.buttons.primary")}
+          </Text>
+        </Button>
+        <Button
+          flex={1}
+          gradient={gradients.secondary}
+          marginBottom={sizes.base}
+        >
+          <Text white bold transform="uppercase">
+            {t("componentExamples.buttons.secondary")}
+          </Text>
+        </Button>
+        <Button flex={1} gradient={gradients.info} marginBottom={sizes.base}>
+          <Text white bold transform="uppercase">
+            {t("componentExamples.buttons.info")}
+          </Text>
+        </Button>
+        <Button flex={1} gradient={gradients.success} marginBottom={sizes.base}>
+          <Text white bold transform="uppercase">
+            {t("componentExamples.buttons.success")}
+          </Text>
+        </Button>
+        <Button flex={1} gradient={gradients.warning} marginBottom={sizes.base}>
+          <Text white bold transform="uppercase">
+            {t("componentExamples.buttons.warning")}
+          </Text>
+        </Button>
+        <Button flex={1} gradient={gradients.danger} marginBottom={sizes.base}>
+          <Text white bold transform="uppercase">
+            {t("componentExamples.buttons.danger")}
+          </Text>
+        </Button>
+        <Button flex={1} gradient={gradients.dark} marginBottom={sizes.base}>
+          <Text white bold transform="uppercase">
+            {t("componentExamples.buttons.dark")}
+          </Text>
+        </Button>
+        <Block row justify="space-between" marginBottom={sizes.base}>
+          <Button
+            flex={1}
+            row
+            gradient={gradients.dark}
+            onPress={() => setModal(true)}
+          >
+            <Block
+              row
+              align="center"
+              justify="space-between"
+              paddingHorizontal={sizes.sm}
+            >
+              <Text white bold transform="uppercase" marginRight={sizes.sm}>
+                {quantity}
+              </Text>
+              <Image
+                source={assets.arrow}
+                color={colors.white}
+                transform={[{ rotate: "90deg" }]}
+              />
+            </Block>
+          </Button>
+          <Button flex={5} gradient={gradients.dark} marginHorizontal={sizes.s}>
+            <Text white bold transform="uppercase" marginHorizontal={sizes.s}>
+              {t("componentExamples.buttons.delete")}
+            </Text>
+          </Button>
         </Block>
       </Block>
-    );
-  };
-  
-  export default ComponentExamples;
-  
+      <Modal visible={showModal} onRequestClose={() => setModal(false)}>
+        <FlatList
+          keyExtractor={(index) => `${index}`}
+          style={styles.modal}
+          data={[
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+          ]}
+          renderItem={({ item }) => (
+            <Button
+              marginBottom={sizes.sm}
+              onPress={() => {
+                setQuantity(item);
+                setModal(false);
+              }}
+            >
+              <Text p white semibold transform="uppercase">
+                {item}
+              </Text>
+            </Button>
+          )}
+        />
+      </Modal>
+    </Block>
+  );
+};
+
+const ComponentExamples = () => {
+  const { assets, sizes } = useTheme();
+  const navigation = useNavigation();
+  const headerHeight = useHeaderHeight();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackground: () => (
+        <Image
+          radius={0}
+          resizeMode="cover"
+          width={sizes.width}
+          height={headerHeight}
+          source={assets.header}
+        />
+      ),
+    });
+  }, [assets.header, navigation, sizes.width, headerHeight]);
+
+  return (
+    <Block safe>
+      <Block
+        scroll
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingVertical: sizes.padding }}
+      >
+        <Block>
+          <Buttons />
+        </Block>
+      </Block>
+    </Block>
+  );
+};
+
+export default ComponentExamples;
+
+const styles = StyleSheet.create({
+  modal: {
+    maxHeight: 250,
+    height: 250,
+  },
+});
